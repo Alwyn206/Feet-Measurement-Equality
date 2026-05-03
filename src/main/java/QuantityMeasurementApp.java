6e79911e64babaa264a3fdd349e9afcd2f968855
ALWYN BARNABAS DANIELS <ad0273@srmist.edu.in>
2026-05-03 12:10:58 +0530

    Implement UC2: Feet and Inches measurement equality

--- src/main/java/QuantityMeasurementApp.java
+++ src/main/java/QuantityMeasurementApp.java
@@ -16,12 +16,40 @@ public class QuantityMeasurementApp {
         }
     }
 
-    public static void main(String[] args) {
-        Feet feet1 = new Feet(1.0);
-        Feet feet2 = new Feet(1.0);
+    public static class Inches {
+        private final double value;
 
+        public Inches(double value) {
+            this.value = value;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            Inches inches = (Inches) obj;
+            return Double.compare(inches.value, value) == 0;
+        }
+    }
+
+    public static void checkFeetEquality(double val1, double val2) {
+        Feet feet1 = new Feet(val1);
+        Feet feet2 = new Feet(val2);
         boolean result = feet1.equals(feet2);
-        System.out.println("Input: 1.0 ft and 1.0 ft");
+        System.out.println("Input: " + val1 + " ft and " + val2 + " ft");
         System.out.println("Output: Equal (" + result + ")");
+    }
+
+    public static void checkInchesEquality(double val1, double val2) {
+        Inches inches1 = new Inches(val1);
+        Inches inches2 = new Inches(val2);
+        boolean result = inches1.equals(inches2);
+        System.out.println("Input: " + val1 + " inch and " + val2 + " inch");
+        System.out.println("Output: Equal (" + result + ")");
+    }
+
+    public static void main(String[] args) {
+        checkInchesEquality(1.0, 1.0);
+        checkFeetEquality(1.0, 1.0);
     }
 }
